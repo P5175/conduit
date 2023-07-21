cfd1a03fc294800fd9e8dd94e55db267dfb55538
p b <pbbbb264@gmail.com>
2023-07-21 18:58:58 +0530

    seven

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,154 +1,86 @@
-// import { Component, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
-// import { FormArray, FormBuilder, Validators } from '@angular/forms';
-// import { first } from 'rxjs';
-// import { Card } from 'src/model/card.model';
-
-// @Component({
-//   selector: 'app-editor',
-//   templateUrl: './editor.component.html',
-//   styleUrls: ['./editor.component.css'],
-//   encapsulation:ViewEncapsulation.None
-// })
-// export class EditorComponent  {
-//   @ViewChild('tagContainer',{static:true}) tagContainer!:ElementRef<HTMLDivElement>;
-//   articleForm;
-//   smalltag = "";
-//   cardId = 1;
-//   card: Card = {
-//     cardId: 0,
-//     authorId: 0,
-//     title: '',
-//     description: '',
-//     tags: [],
-//     date: '',
-//     likes: 0
-//   }
-//   constructor(private formbuilder: FormBuilder) {
-//     this.articleForm = this.formbuilder.group({
-//       title: ["", [Validators.required]],
-//       description: ["", [Validators.required]],
-//       tags: [""]
-//     })
-//   };
-
-// addtag(temp:string){
-// //  console.log(this.tagContainer.nativeElement);
-// //  console.log(temp);
- 
-// const tag=document.createElement('span');
-
-
-// tag.innerText=temp;
-// this.tagContainer.nativeElement.appendChild(tag);
-//   }
-
-
-  
-//   onSubmit() {
-//     this.card.cardId = this.cardId;
-//     this.card.authorId = Number(localStorage.getItem('userid'));
-//     this.card.title = this.articleForm.value.title ?? "";
-//     this.card.description = this.articleForm.value.description ?? "";
-//     this.card.date = new Date().toISOString();
-
-//     this.cardId++;
-//   }
-
-//   enter(event: any) {
-//     // console.log(event);
-// // event.preventdefault();
-//     if (event.key === "Enter" && this.smalltag.trim() != "") {
-
-//       this.card.tags.push(this.smalltag.trim());
-//       this.addtag(this.smalltag?.trim());
-//       this.articleForm.get('tags')?.reset();
-    
-
-//     }
-//     if (event.key === "Enter" && this.smalltag?.trim() == "") {
-//       this.articleForm.get('tags')?.reset();
-//     }
-//     console.log(this.card.tags);
-
-
-//   }
-
-
-// }
 import { Component, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
 import { FormArray, FormBuilder, Validators } from '@angular/forms';
 import { first } from 'rxjs';
-import { Card } from 'src/model/card';
+import { Card } from 'src/model/card.model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.None
+  selector: 'app-editor',
+  templateUrl: './editor.component.html',
+  styleUrls: ['./editor.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
-  @ViewChild('tagContainer', { static: true }) tagContainer!: ElementRef<HTMLDivElement>;
-  articleForm;
-  smalltag = "";
-  cardId = 1;
-  card: Card = {
-    cardId: 0,
-    authorId: 0,
-    title: '',
-    description: '',
-    tags: [],
-    date: '',
-    likes: 0
-  }
-  constructor(private formbuilder: FormBuilder) {
-    this.articleForm = this.formbuilder.group({
-      title: ["", [Validators.required]],
-      description: ["", [Validators.required]],
-      tags: [""]
-    })
-  };
+export class EditorComponent {
+  @ViewChild('tagContainer', { static: true }) tagContainer!: ElementRef<HTMLDivElement>;
+  articleForm;
+  smalltag = "";
+  cardId = 1;
+  card: Card = {
+    cardId: 0,
+    authorId: 0,
+    title: '',
+    description: '',
+    tags: [],
+    date: '',
+    likes: 0
+  }
+  constructor(private formbuilder: FormBuilder) {
+    this.articleForm = this.formbuilder.group({
+      title: ["", [Validators.required]],
+      description: ["", [Validators.required]],
+      tags: [""]
+    })
+  };
 
-  addtag(temp: string) {
-    //  console.log(this.tagContainer.nativeElement);
-    //  console.log(temp);
+  addtag(temp: string) {
+    //  console.log(this.tagContainer.nativeElement);
+    //  console.log(temp);
 
-    const tag = document.createElement('span');
+    const tag = document.createElement('span');
+tag.ondblclick=()=>this.removetag(tag);
 
-
-    tag.innerText = temp;
-    this.tagContainer.nativeElement.appendChild(tag);
-  }
-
-
-
-  onSubmit() {
-    this.card.cardId = this.cardId;
-    this.card.authorId = Number(localStorage.getItem('userid'));
-    this.card.title = this.articleForm.value.title ?? "";
-    this.card.description = this.articleForm.value.description ?? "";
-    this.card.date = new Date().toISOString();
-
-    this.cardId++;
-  }
-
-  enter(event: any) {
-    // console.log(event);
-    // event.preventdefault();
-    if (event.key === "Enter" && this.smalltag.trim() != "") {
-
-      this.card.tags.push(this.smalltag.trim());
-      this.addtag(this.smalltag?.trim());
-      this.articleForm.get('tags')?.reset();
+    tag.innerText = temp;
+    this.tagContainer.nativeElement.appendChild(tag);
+  }
+removetag(tag:HTMLElement){
+const tagtext=tag.innerText;
+this.card.tags=this.card.tags.filter((t)=>t!==tagtext);
+}
 
 
-    }
-    if (event.key === "Enter" && this.smalltag?.trim() == "") {
-      this.articleForm.get('tags')?.reset();
-    }
-    console.log(this.card.tags);
+  onSubmit() {
+    this.card.cardId = this.cardId;
+    this.card.authorId = Number(localStorage.getItem('userid'));
+    this.card.title = this.articleForm.value.title ?? "";
+    this.card.description = this.articleForm.value.description ?? "";
+    this.card.date = new Date().toISOString();
+
+    this.cardId++;
+  }
+
+  enter(event: any) {
+    // console.log(event);
+    // event.preventdefault();
+    if (event.key === "Enter" && this.smalltag.trim() != "") {
+      if (this.card.tags.includes(this.smalltag.trim())) {
+        this.articleForm.get('tags')?.reset();
+      } else {
+
+        this.card.tags.push(this.smalltag.trim());
+
+        this.addtag(this.smalltag?.trim());
+        this.articleForm.get('tags')?.reset();
+      }
 
 
-  }
+    }
+    if (event.key === "Enter" && this.smalltag?.trim() == "") {
+      this.articleForm.get('tags')?.reset();
+    }
+    console.log(this.card.tags);
+
+
+  }
 
 
 }
+
